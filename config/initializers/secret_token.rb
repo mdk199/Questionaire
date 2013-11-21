303edffa376c4e5348c2add5debe33f54406abe1
ubuntu <ubuntu@ubuntu-OptiPlex-390.(none)>
2013-11-21 21:01:04 +0530

    master basic files for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorproject::Application.config.secret_token = '707cee24a87bd2a8ea5480620a1647d14b26024860776b6d9f616abe2b40902547d640c8fbc5598f03552f91f54ddf2c9680588c5132efe9be739494b2377312'
