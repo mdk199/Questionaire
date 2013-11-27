5e60c9d8064ed8e69fb7ae6d250c45ccac0896a5
mdk199 <mdk199@gmail.com>
2013-11-27 15:57:11 +0530

    Delete 20131125141400_alter_table_answers.rb

--- db/migrate/20131125141400_alter_table_answers.rb
+++
@@ -1,7 +0,0 @@
-class AlterTableAnswers < ActiveRecord::Migration
-  def change
-    add_column :answers, :question_id, :integer
-    add_column :answers, :approved, :boolean, :default => false
-    add_column :answers, :flagged, :boolean, :default => false
-  end
-end
