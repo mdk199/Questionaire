3932176aae6e323d3b67aeccece9f9357604d0d0
sabyasachi-paul <spcslg@gmail.com>
2013-12-04 20:07:49 +0530

    Updating validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,24 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :answer
-  attr_accessible :answer_id, :comment, :user_id
-  validates_presence_of :answer_id, :user_id, :comment
+	belongs_to :user
+	belongs_to :answer
+	validates_presence_of :comment
+	attr_accessible :answer_id, :comment, :user_id
+	validates_presence_of :user_id
+  	validates_presence_of :answer_id
+  	validate :valid_user, :valid_answer
+  	def valid_user
+  		if self.user_id.present?
+	  		unless User.find_by_id(self.user_id).present?
+	  			self.errors.add(:user_id, "not found!")
+	  		end
+	  	end
+  	end
+
+  	def valid_answer
+  		if self.answer_id.present?
+	  		unless Answer.find_by_id(self.answer_id).present?
+	  			self.errors.add(:answer_id, "not found!")
+	  		end
+	  	end
+  	end
 end
