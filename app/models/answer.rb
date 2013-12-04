4109a0f93006bc07596b5082a9b89eb295ba90ee
sabyasachi-paul <spcslg@gmail.com>
2013-12-04 20:02:54 +0530

    Updating validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,6 +6,24 @@ class Answer < ActiveRecord::Base
 	has_many :flags, as: :flagable
 	has_many :comments
 	attr_accessible :answer, :user_id, :question_id
-  	validates_presence_of :answer, :user, :question
+	validates :answer, :presence => true
+	validates_presence_of :user_id
+  	validates_presence_of :question_id
+  	validate :valid_user, :valid_question
 
+  	def valid_user
+  		if self.user_id.present?
+	  		unless User.find_by_id(self.user_id).present?
+	  			self.errors.add(:user_id, "not found!")
+	  		end
+	  	end
+  	end
+
+  	def valid_question
+  		if self.question_id.present?
+	  		unless Question.find_by_id(self.question_id).present?
+	  			self.errors.add(:question_id, "not found!")
+	  		end
+	  	end
+  	end
 end
