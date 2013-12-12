363550bbd6b177d1ba3d184565f5ca4db3888cc5
Mandeep Kumawat <mdk199@gmail.com>
2013-12-12 16:39:18 +0530

    Question_controller modified according to the user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.find(:all, :include=>"answers")
+    @questions = Question.where(:user_id=>current_user.id).find(:all, :include=>"answers")
 
     respond_to do |format|
       format.html # index.html.erb
