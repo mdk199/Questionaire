4225bd037e32b5255836a60a123523a14eba3d0b
Mandeep Kumawat <mdk199@gmail.com>
2013-12-16 17:40:10 +0530

    configuration for mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "username@gmail.com",
-  :password             => "yourpassword",
+  :user_name            => "potaliproject@gmail.com",
+  :password             => "84pass26",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
