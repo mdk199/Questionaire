ab17c3db176fdc38b03e2f63ee93ec96dbd8674c
sabyasachi-paul <spcslg@gmail.com>
2013-12-16 14:58:44 +0530

    potali-5-1 welcome layout changed.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,11 @@
 class WelcomeController < ApplicationController
   layout "dashboard"
+
+	def index
+	@questions = Question.where(:user_id=>current_user.id).order(:answers_count=>"DESC").limit(5).find(:all, :include=>"answers")
+	respond_to do |format|
+		format.html # index.html.erb
+		format.json { render json: @questions }
+		end
+	end
 end
