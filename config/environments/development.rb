5590a3bf98e83751112f7b72ee432769debea466
Gaurav Soni <raveformails@gmail.com>
2013-12-16 16:57:20 +0530

    potali-5-1 mailer config and misc. changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rorproject::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -34,5 +34,5 @@ Rorproject::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3001' }
 end
