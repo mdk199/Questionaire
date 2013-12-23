a4252f56ecf74da80c88c4af4f77e9d4c0f45a02
Mandeep Kumawat <mdk199@gmail.com>
2013-12-23 21:11:01 +0530

    potali-5-1 expertise splitting issue handled and Flags fixed

--- app/views/questions/flag.js
+++ app/views/questions/flag.js
@@ -1,1 +1,3 @@
+$("#flag_<%= @question.id %>").attr('style','color:red;');
+$("#flag_<%= @question.id %>").attr('href','/questions/<%= @question.id %>/unflag');
 alert("We have taken your request to flag this question");
--- app/views/questions/unflag.js
+++ app/views/questions/unflag.js
@@ -1,1 +1,3 @@
 alert("We have taken your request to unflag this question");
+$("#flag_<%= @question.id %>").attr('style','');
+$("#flag_<%= @question.id %>").attr('href','/questions/<%=@question.id%>/flag');
