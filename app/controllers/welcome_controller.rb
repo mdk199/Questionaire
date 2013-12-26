b44072475b38f05bf8ac1314cc27511284efd13e
Mandeep Kumawat <mdk199@gmail.com>
2013-12-26 11:51:27 +0530

    Potali-5-1 tabs_on_rails installed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,6 @@ class WelcomeController < ApplicationController
 
 	def index
 	@questions = Question.where(:user_id=>current_user.id).order(:answers_count=>"DESC").limit(5).find(:all, :include=>"answers")
-	@expertise = Expertise.find_by_user_id(current_user.id)
-    k = @expertise.expertise.split(",");
-  
-    
 	@expertisequestions = Question.where(:user_id=>current_user).order(:answers_count=>"DESC").limit(5).find(:all, :include=>"answers")
 	respond_to do |format|
 		format.html # index.html.erb
