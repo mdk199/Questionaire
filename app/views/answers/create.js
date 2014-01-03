e6803407fbdac7f5f5faa85e1a8eedceb662456e
Mandeep Kumawat <mdk199@gmail.com>
2014-01-03 18:15:52 +0530

    potali-5-1 approved answer link created

--- app/views/answers/create.js
+++ app/views/answers/create.js
@@ -2,6 +2,7 @@
   CKEDITOR.instances.editor_<%= @answer.question.id%>.setData('');
   $("#panel_answer_<%= @answer.question.id%>").remove();
   $('#add_answer_<%= @answer.question.id%>').attr('disabled', false);
+  $("#new_answer_<%= @answer.question.id%>").remove();
   // CKEDITOR.instances.editor_<%= @answer.question.id%>.destroy();
   $("#answers_<%= @answer.question.id%>").append("<%= escape_javascript( render 'answers/answer', :answer => @answer ) %>");
   $("#question_<%= @answer.question.id%> form")[0].reset();
