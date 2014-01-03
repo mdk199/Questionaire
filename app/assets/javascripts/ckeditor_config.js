454dca2e13b4b4fc32c86afc3ef169dd305f0783
sabyasachi-paul <spcslg@gmail.com>
2014-01-03 17:03:09 +0530

    ckeditor changed

--- app/assets/javascripts/ckeditor_config.js
+++ app/assets/javascripts/ckeditor_config.js
@@ -1,8 +1,6 @@
 CKEDITOR.config.toolbar = [
-   ['Styles','Format','Font','FontSize'],
-   '/',
+   ['Styles','Format','Font','FontSize'],   ['Image','Table','-','Link','Flash','Smiley','TextColor','BGColor','Source'],
    ['Bold','Italic','Underline','StrikeThrough','-','Undo','Redo','-','Cut','Copy','Paste','Find','Replace','-','Outdent','Indent','-','Print'],
-   '/',
-   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-   ['Image','Table','-','Link','Flash','Smiley','TextColor','BGColor','Source']
+   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock']
+
 ] ;
