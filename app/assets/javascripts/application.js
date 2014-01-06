8a0e0a8a8ee98c090dc6b66d601b805748fb37f8
sabyasachi-paul <spcslg@gmail.com>
2014-01-06 12:34:06 +0530

    tags changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,28 +5,28 @@
 $(function(){
 
 $('#tags').tagsInput({
-  "width": "75%",
-  "height": "70px",
+  "width": "100%",
+  "height": "50%",
   'defaultText':''
 }); 
 
 $('#tags2').tagsInput({
-  'width': '90%',
-  'height':'70px',
-  'defaultText':'Add Tag'
+  'width': '80%',
+  'height': "50%",
+  'defaultText':'Expertise'
 }); 
 
 $('#tags3').tagsInput({
   'width': '90%',
-  'height':'70px',
+  'height': "50%",
   'defaultText':'Add Tag',
 'autocomplete_url':'/2013/js/jQuery-Tags-Input/test/fake_json_endpoint.html' // jquery ui autocomplete requires a json endpoint
 });
 
 $('#tags4').tagsInput({
-  'width': '90%',
-  'height':'70px',
-  'defaultText':'Add Tag'
+  'width': '80%',
+  'height': "50%",
+  'defaultText':'Interest'
 }); 
 
 $('#tags_tag').focus();
