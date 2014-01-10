fbe7ea8c37b30ef963da85e92303d66eb53081b7
Gaurav Soni <raveformails@gmail.com>
2014-01-10 16:09:32 +0530

    potali-5-1 UI modification

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,10 @@
 class Tag < ActiveRecord::Base
+  TAG_TYPES = { 
+                0=>"default",
+                1=>"primary",
+                2=>"success",
+                3=>"info",
+                4=>"warning",
+                5=>"danger",
+              }
 end
