be23405c917bcb9fc1efad4f969511bfa084c990
sabyasachi-paul <spcslg@gmail.com>
2014-01-10 21:05:30 +0530

    flag fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -113,7 +113,7 @@ class QuestionsController < ApplicationController
 
   def flag
     @question = Question.find(params[:id])
-    Flag.add_flaga(@question, current_user)
+    Flag.add_flag(@question, current_user)
     respond_to do |format|
       format.js { render "questions/flag", :layout => false }
       format.json { render json: @questions }
