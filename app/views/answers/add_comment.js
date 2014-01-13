050f4e7f022eb483b7a31c6201bda95325f48ea0
Mandeep Kumawat <mdk199@gmail.com>
2014-01-13 09:52:23 -0800

    potali-5-1 comment issue totally fixed

--- app/views/answers/add_comment.js
+++ app/views/answers/add_comment.js
@@ -1,5 +1,4 @@
-$("#comment_<%= @answer.id%>").attr("href","#");
-$("#comment_<%= @answer.id%>").attr("data-toggle","modal");
-$("#comment_<%= @answer.id%>").attr("data-target","#modal-window");
-$('#modal-content_<%= @answer.id%>').html("<%=escape_javascript(render 'comments/form', :answer => @answer)%>" );
-$('#comment_<%= @answer.id%>').click();
+$("#comment").attr("data-toggle","modal");
+$("#comment").attr("data-target","#modal-window");
+$("#modal-content").html("<%=escape_javascript(render 'comments/form', :answer => @answer)%>" );
+$("#comment").click();
--- app/views/comments/create.js
+++ app/views/comments/create.js
@@ -1,10 +1,10 @@
 <% if @comment.valid? %>
+  $("#modal-close").click();
   $("#panel_comment_<%= @comment.answer.id%>").remove();
   $("#comments_<%= @comment.answer.id%>").append("<%= escape_javascript( render 'comments/comment', :comment => @comment ) %>");
-  $("#questionpanel_<%= @comment.answer.question.id%> form").before("<%= escape_javascript(show_notice(notice)) %>");
-  $("#new_comment_<%= @comment.answer.id%>").remove();
+  $("#comments_<%= @comment.answer.id%>").before("<%= escape_javascript(show_notice(notice)) %>");
   $(".alert").fadeOut(5000);
 <% else %>
-  $("#questionpanel_<%= @comment.answer.question.id%> form").before("<%= escape_javascript(show_alert(@comment.errors.full_messages.join(", "))) %>");
+  $(".modal-body").before("<%= escape_javascript(show_alert(@comment.errors.full_messages.join(", "))) %>");
   $(".alert").fadeOut(5000);
 <% end %>
