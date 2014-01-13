d359d2989b174ef5d42a662581e8a29bb66436b6
Mandeep Kumawat <mdk199@gmail.com>
2014-01-13 19:13:40 +0530

    potali-5-1 add answer fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   layout 'devise'
   before_filter :authenticate_user!
 
-  rescue_from 'CanCan::AccessDenied' do |exception|
-    respond_to do |format|
-      format.html do
-        flash[:error] = exception.message
-        redirect_to welcome_path
-      end
-      format.js { render :js => "alert('#{exception.message}');" }
-    end
-  end
+  # rescue_from 'CanCan::AccessDenied' do |exception|
+  #   respond_to do |format|
+  #     format.html do
+  #       flash[:error] = exception.message
+  #       redirect_to welcome_path
+  #     end
+  #     format.js { render :js => "alert('#{exception.message}');" }
+  #   end
+  # end
 
   def after_sign_in_path_for(resource_or_scope)
     welcome_path
--- app/views/questions/add_answer.js
+++ app/views/questions/add_answer.js
@@ -1,2 +1,2 @@
+$('#answers_<%= @question.id%>').append("<%= escape_javascript(render 'answers/form', :question => @question)%>" );
 $('#add_answer_<%= @question.id%>').attr('disabled', true);
-$('#answers_<%= @question.id%>').append("<%=escape_javascript(render 'answers/form', :question => @question)%>" );
