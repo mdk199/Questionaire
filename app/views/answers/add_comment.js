5d2f689383269a24e45663b0df8520bd90038dba
Mandeep Kumawat <mdk199@gmail.com>
2014-01-13 20:35:31 +0530

    potali-5-1 Modal work in progress

--- app/views/answers/add_comment.js
+++ app/views/answers/add_comment.js
@@ -1,2 +1,1 @@
-$('#add_comment_<%= @answer.id%>').attr('disabled', true);
-$ ('#comments_<%= @answer.id%>').append("<%=escape_javascript(render 'comments/form', :answer => @answer)%>" )
+$ ('#modal-content_<%= @answer.id%>').html("<%=escape_javascript(render 'comments/form', :answer => @answer)%>" );
