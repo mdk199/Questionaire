ad24a6e53f2ccfee7c6fa098d1c4506c2622a7c8
Mandeep Kumawat <mdk199@gmail.com>
2014-01-13 21:17:08 +0530

    potali-5-1 add_comment in progress

--- app/views/answers/add_comment.js
+++ app/views/answers/add_comment.js
@@ -1,1 +1,5 @@
-$ ('#modal-content_<%= @answer.id%>').html("<%=escape_javascript(render 'comments/form', :answer => @answer)%>" );
+$("#comment_<%= @answer.id%>").attr("href","#");
+$("#comment_<%= @answer.id%>").attr("data-toggle","modal");
+$("#comment_<%= @answer.id%>").attr("data-target","#modal-window");
+$('#modal-content_<%= @answer.id%>').html("<%=escape_javascript(render 'comments/form', :answer => @answer)%>" );
+$('#comment_<%= @answer.id%>').click();
