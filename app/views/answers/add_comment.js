95e7d21b81f3c6f4788fa2209237d314e64733e2
Mandeep Kumawat <mdk199@gmail.com>
2014-01-14 09:05:34 +0530

    potali-5-1 shared modal create for use in sign-in and sign-up

--- app/views/answers/add_comment.js
+++ app/views/answers/add_comment.js
@@ -1,4 +1,2 @@
-$("#comment").attr("data-toggle","modal");
-$("#comment").attr("data-target","#modal-window");
 $("#modal-content").html("<%=escape_javascript(render 'comments/form', :answer => @answer)%>" );
-$("#comment").click();
+$("#modal-show").click();
