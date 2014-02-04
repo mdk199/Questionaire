4b0991993a8d8814445fbf16e62126ea47ee0982
sabyasachi-paul <spcslg@gmail.com>
2014-02-04 16:38:50 +0530

    changes to activity log

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -56,5 +56,62 @@ describe "the signin process" do
 		        expect(page).to have_content "Listing Blocked Questions"
 	    	end
 	    end
+
+	    context "can manage activity of all users" do
+	        before(:each) do
+	        	@admin_user.update_attribute(:confirmation_token, nil)
+	        	@admin_user.update_attribute(:confirmed_at, Time.now)
+	      	end
+
+	    	it "have access to all blocked_questions after sign-in" do
+	    		within("#new_user") do
+	          		fill_in 'user_username', :with => @admin_user.username
+	         		fill_in 'user_password', :with => @admin_user.password
+	       		 end
+		        click_button 'Sign in'
+		        expect(page).to have_content success_msg 
+		        find_link('Activity Log').visible?.should == true
+		        click_link('Activity Log')
+		        expect(page).to have_content "Activity Log"
+	    	end
+	    end
+
+	    context "can manage all users" do
+	        before(:each) do
+	        	@admin_user.update_attribute(:confirmation_token, nil)
+	        	@admin_user.update_attribute(:confirmed_at, Time.now)
+	      	end
+
+	    	it "have access to all blocked_questions after sign-in" do
+	    		within("#new_user") do
+	          		fill_in 'user_username', :with => @admin_user.username
+	         		fill_in 'user_password', :with => @admin_user.password
+	       		 end
+		        click_button 'Sign in'
+		        expect(page).to have_content success_msg 
+		        find_link('Manage Users').visible?.should == true
+		        click_link('Manage Users')
+		        expect(page).to have_content "Listing Users"
+	    	end
+	    end
+
+	    context "can manage all tags" do
+	        before(:each) do
+	        	@admin_user.update_attribute(:confirmation_token, nil)
+	        	@admin_user.update_attribute(:confirmed_at, Time.now)
+	      	end
+
+	    	it "have access to all blocked_questions after sign-in" do
+	    		within("#new_user") do
+	          		fill_in 'user_username', :with => @admin_user.username
+	         		fill_in 'user_password', :with => @admin_user.password
+	       		 end
+		        click_button 'Sign in'
+		        expect(page).to have_content success_msg 
+		        find_link('Tag Log').visible?.should == true
+		        click_link('Tag Log')
+		        expect(page).to have_content "Listing All Tags"
+	    	end
+	    end
 	end
 end
